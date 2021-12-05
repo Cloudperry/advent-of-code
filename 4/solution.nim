8eac2a57768348baced44f3a68d0ed7da5b135de
Roni Hokkanen <roni.hokkanen@protonmail.com>
2021-12-06 00:56:42 +0200

    Small changes to lib.nim (provide a way to use alternate input). Add day 4. Day 4 is now a single executable and takes in parameters for which part to solve using what input.

---
+++ 4/solution.nim
@@ -0,0 +1,79 @@
+import ../lib
+import cligen
+
+type
+  BingoBoard = array[0 .. 4, array[0 .. 4, int]]
+  BoardPositions = HashSet[tuple[row, col: int]]
+  BingoWinData = tuple[boardI, lastDrawnNum: int; markedNumbers: BoardPositions]
+  Bingo = object
+    numbersDrawn: seq[int]
+    boards: seq[BingoBoard]
+
+const emptyBoard = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]].BingoBoard
+const diagWin1: BoardPositions = collect(for i in 0 .. 4: {(i, i)})
+const diagWin2: BoardPositions = collect(for i in 0 .. 4: {(4-i, i)})
+
+proc parseBingo(input: string): Bingo =
+  let lines = input.splitLines()
+  var boardCurrentLine: int
+  for i, line in lines:
+    if i == 0:
+      var matches: RegexMatch
+      if line.match(re"(?:(\d+),*)+", matches):
+        result.numbersDrawn = matches.group(0, input).mapIt(it.parseInt)
+    else:
+      var col0, col1, col2, col3, col4: int
+      if line == "":
+        result.boards.add emptyBoard
+        boardCurrentLine = 0
+      elif line.scanf("$s$i$s$i$s$i$s$i$s$i", col0, col1, col2, col3, col4): #What a mess :)
+        result.boards[^1][boardCurrentLine] = [col0, col1, col2, col3, col4]
+        boardCurrentLine += 1
+
+iterator coordAndValue(b: BingoBoard): tuple[row, col, value: int] =
+  for rowN, row in b:
+    for colN, value in row:
+      yield (rowN, colN, value)
+
+proc getRowPositions(row: int): BoardPositions =
+  for col in 0 .. 4: result.incl (row, col)
+proc getColPositions(col: int): BoardPositions =
+  for row in 0 .. 4: result.incl (row, col)
+
+template markBoardsDo(b: Bingo; code: untyped) =
+  var markedBoards {.inject.} = newSeq[BoardPositions](b.boards.len)
+  for winningNumber {.inject.} in b.numbersDrawn:
+    for i {.inject.}, board in b.boards:
+      for rowN, colN, number in board.coordAndValue:
+        if number == winningNumber:
+          markedBoards[i].incl (row: rowN, col: colN)
+          if diagWin1 <= markedBoards[i] or diagWin2 <= markedBoards[i] or
+          getRowPositions(rowN) <= markedBoards[i] or getColPositions(colN) <= markedBoards[i]:
+            code
+
+proc findWinningBoard(b: Bingo): BingoWinData =
+  b.markBoardsDo:
+    return (i, winningNumber, markedBoards[i])
+
+proc findLastToWinBoard(b: Bingo): BingoWinData =
+  var wonBoards: HashSet[int]
+  b.markBoardsDo:
+    wonBoards.incl i
+    if wonBoards.len == b.boards.len:
+      return (i, winningNumber, markedBoards[i])
+
+proc getScore(b: Bingo, w: BingoWinData): int =
+  for row, col, number in b.boards[w.boardI].coordAndValue:
+    if (row, col) notin w.markedNumbers: result += number
+  result *= w.lastDrawnNum
+
+proc solvePart(part = 1.AocPart, inputFile = "") =
+  let bingo = getInputWithParser(4, 2021, parseBingo, inputFile)
+  if part == 1:
+    let winningBoardData = bingo.findWinningBoard()
+    echo getScore(bingo, winningBoardData)
+  else:
+    let winningBoardData = bingo.findLastToWinBoard()
+    echo getScore(bingo, winningBoardData)
+
+if isMainModule: dispatch(solvePart)
--- lib.nim
+++ lib.nim
@@ -1,28 +1,44 @@
 import os, httpclient
-import math, strutils, strformat, strscans, sequtils, tables, sets, hashes, times
+import math, strutils, strformat, strscans, sequtils, tables, sets, hashes, times, sugar, options
 import regex, bitty
-export math, strutils, strformat, strscans, sequtils, tables, sets, hashes, times
+export math, strutils, strformat, strscans, sequtils, tables, sets, hashes, times, sugar, options
 export regex, bitty
 
-let currentYear = now().year
+type
+  AocPart* = range[1 .. 2]
+  AocDay* = range[1 .. 25]
+  AocYear* = range[2015 .. int.high]
 
-proc getInputIfNotDownloaded*(day: Natural; year = currentYear) =
+proc getInputIfNotDownloaded(day: AocDay; year: AocYear) =
   if not fileExists("input.txt"):
     var client = newHttpClient()
     let sessionCookie = readFile("../sessionCookie").strip
-    client.headers = newHttpHeaders({ "Cookie": "session={sessionCookie}" })
-    echo fmt"downloading https://adventofcode.com/{year}/day/{day}/input with {sessionCookie}"
+    client.headers = newHttpHeaders({ "Cookie": fmt"session={sessionCookie}" })
+    echo fmt"downloading https://adventofcode.com/{year}/day/{day}/input"
     client.downloadFile(fmt"https://adventofcode.com/{year}/day/{day}/input", "input.txt")
 
-proc getInputLines*[T](day: Natural; year = currentYear): seq[T] =
-  getInputIfNotDownloaded(day, year)
-  for line in lines("input.txt"): result.add line
+proc getInput*(day: AocDay, year: AocYear; inputFile = ""): string =
+  if not inputFile.isEmptyOrWhitespace:
+    result = readFile(inputFile)
+  else:
+    getInputIfNotDownloaded(day, year)
+    result = readFile("input.txt")
+  if result[^1] in Whitespace: result.delete(result.high .. result.high)
 
-proc getInputLinesWithParser*[T](day: Natural; parseWith: proc (input: string): T; year = currentYear): seq[T] =
-  getInputIfNotDownloaded(day, year)
-  for line in lines("input.txt"):
-    result.add line.parseWith
+proc getInputLines*(day: AocDay, year: AocYear; inputFile = ""): seq[string] =
+  let inputRaw = getInput(day, year, inputFile)
+  for line in splitLines(inputRaw): result.add line
 
-proc getInputWithParser*[T](day: Natural; parseWith: proc (input: string): T; year = currentYear): T =
-  getInputIfNotDownloaded(day, year)
-  readFile("input.txt").parseWith
+proc getInputLinesWithParser*[T](day: AocDay, year: AocYear; parser: proc (input: string): T;
+                                 inputFile = ""): seq[T] =
+  let inputRaw = getInput(day, year, inputFile)
+  for line in splitLines(inputRaw): result.add line.parser
+
+proc getInputWithParser*[T](day: AocDay, year: AocYear; parser: proc (input: string): T;
+                            inputFile = ""): T =
+  let inputRaw = getInput(day, year, inputFile)
+  result = inputRaw.parser
+
+#TODO: Make day 3 solution be able to use alternate input using inputFile
+#TODO: Add automatic sending (and checking) of answers and make it easy to integrate with cligen arguments
+#TODO: Separate exporting of commonly used libraries into some categories and put them in their own files and rename this file to aocUtils or something
