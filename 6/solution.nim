69279abeb7ebe4415734566135677384d73b1a4c
Roni Hokkanen <roni.hokkanen@protonmail.com>
2021-12-12 04:23:53 +0200

    Convert day 6 to using array instead of seq.

--- 6/solution.nim
+++ 6/solution.nim
@@ -1,24 +1,25 @@
 import ../lib
 import regex, cligen
 
-proc parseCsvInts(input: string): seq[uint64] =
-  result = newSeq[uint64](9) #Idk why, but I needed uint64 to make it work
+type AgeGroups = array[9, int]
+
+proc parseCsvInts(input: string): AgeGroups =
   var matches: RegexMatch
   if input.match(re"(?:(\d),*)+", matches):
     for lfAge in matches.group(0, input):
       result[lfAge.parseInt].inc
 
-proc lanternFishSpawnSimCycle(lfAgeCounts: var seq[uint64]) =
-  var newLfAges = newSeq[uint64](9)
+proc lanternFishSpawnSimCycle(lfAgeCounts: var AgeGroups) =
+  var newLfAgeCounts: AgeGroups
   for lfAge in 0 .. 8:
     case lfAge #This whole thing could quite easily be rewritten to be branchless with wraparound arithmetic instead of lfAge + 1
     of 0..7:   #That code would require a special case for 6 only, but it would be unreadable to most people (or myself later) so I won't do it
-      newLfAges[lfAge] = lfAgeCounts[lfAge + 1]
-      if lfAge == 6: newLfAges[6] += lfAgeCounts[0]
+      newLfAgeCounts[lfAge] = lfAgeCounts[lfAge + 1]
+      if lfAge == 6: newLfAgeCounts[6] += lfAgeCounts[0]
     of 8:
-      newLfAges[lfAge] = lfAgeCounts[0]
+      newLfAgeCounts[lfAge] = lfAgeCounts[0]
     else: discard
-  lfAgeCounts = newLfAges
+  lfAgeCounts = newLfAgeCounts
 
     
 proc solvePart(part = 1.AocPart; inputFile = "") =
